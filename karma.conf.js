bb4d9ac220126225e44896c73f5d5398aa90e095
Fernando Carrascosa <fcarrascosa@fcarrascosa.es>
2020-09-17 21:08:06 +0200

    Initial Commit

---
+++ karma.conf.js
@@ -0,0 +1,66 @@
+module.exports = config =>
+  config.set({
+    autoWatch: false,
+    browsers: ['ChromeHeadlessNoSandbox'],
+    client: {
+      mocha: {
+        reporter: 'html',
+      },
+    },
+    colors: true,
+    concurrency: Infinity,
+    coverageReporter: {
+      dir: 'coverage',
+      reporters: [
+        { type: 'html' },
+        { type: 'lcovonly' },
+        { type: 'text-summary' },
+      ],
+      thresholds: {
+        global: {
+          branches: 80,
+          functions: 80,
+          lines: 80,
+          statements: 80,
+        },
+      },
+    },
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: [
+          'no-sandbox',
+          '--disable-setuid-sandbox',
+          '--window-size=2560x1440',
+        ],
+      },
+    },
+    esm: {
+      coverage: true,
+      nodeResolve: true,
+    },
+    files: [
+      {
+        pattern: config.grep || 'packages/**/*spec.js',
+        type: 'module',
+      },
+    ],
+    frameworks: ['esm', 'mocha', 'chai', 'source-map-support'],
+    logLevel: config.LOG_INFO,
+    mochaReporter: {
+      showDiff: true,
+    },
+
+    plugins: [
+      '@open-wc/karma-esm',
+      'karma-mocha',
+      'karma-chai',
+      'karma-mocha-reporter',
+      'karma-source-map-support',
+      'karma-coverage',
+      'karma-chrome-launcher',
+    ],
+    reporters: ['mocha', 'coverage'],
+    restartOnFileChange: false,
+    singleRun: true,
+  });
