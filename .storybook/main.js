dd2eaf5a73c17861213a13e8a35dc8c7790b4a50
Fernando Carrascosa <fcarrascosa@fcarrascosa.es>
2020-09-23 00:36:41 +0200

    chore(eslint, storybook): remove sort-import rule as it is inconsistent, also add new files to parse storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,8 @@ module.exports = {
   stories: [
     '../README.md',
     '../stories/**/*.stories.{js,md,mdx}',
-    '../packages/**/stories/**/*.stories.{js,md,mdx}',
+    '../packages/*/README.md',
+    '../packages/*/stories/**/*.stories.{js,md,mdx}',
   ],
   addons: [
     'storybook-prebuilt/addon-knobs/register.js',
