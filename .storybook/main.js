cdb992cdbe16bce976f414cd0964b6490ed0cf09
Fernando Carrascosa <fcarrascosa@fcarrascosa.es>
2020-09-26 21:24:32 +0200

    Initial Commit

---
+++ .storybook/main.js
@@ -0,0 +1,20 @@
+module.exports = {
+  stories: [
+    '../README.md',
+    '../stories/**/*.stories.{js,md,mdx}',
+    '../packages/*/README.md',
+    '../packages/*/stories/**/*.stories.{js,md,mdx}',
+  ],
+  addons: [
+    'storybook-prebuilt/addon-knobs/register.js',
+    'storybook-prebuilt/addon-docs/register.js',
+    'storybook-prebuilt/addon-viewport/register.js',
+    'storybook-prebuilt/addon-a11y/register.js',
+  ],
+  esDevServer: {
+    // custom es-dev-server options
+    nodeResolve: true,
+    watch: true,
+    open: true,
+  },
+};
